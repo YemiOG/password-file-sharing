f23f47cebb4be2d807791cc2ab5c198504691763
Yemi Ogundeyi <yemi.ogundeyi@gmail.com>
2022-08-20 00:12:05 +0100

    completed the project and added refresh functionality button

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import File from "./models/File.js";
 //express multer middleware for uploads destination
 import multer from "multer";
 
+app.use(express.urlencoded({ extended: true }));
+
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
@@ -37,7 +39,31 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
 
-app.get("/file/:id", (req, res) => {});
+app.route("/file/:id").get(handleDownload).post(handleDownload);
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password");
+      return;
+    }
+
+    //if (check password)
+    if (await bcrypt.compare(req.body.password, file.password)) {
+      res.render("password ", { error: true });
+      return;
+    }
+  }
+
+  file.downloadCount++;
+  await file.save();
+
+  console.log(file.downloadCount);
+
+  res.download(file.path, file.originalName);
+}
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on localhost:3000 ");
